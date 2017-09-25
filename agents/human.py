fd4cf88f39c91dcd863bdeaaad09788b274afc10
Ray Barker <21300048@student.uwa.edu.au>
2017-09-25 14:57:22 +0800

    Started splitting cars.py file into vehicle and human files

---
+++ agents/human.py
@@ -0,0 +1,100 @@
+from collections import deque
+
+class Human(object):
+    
+    agent_name = 'Human'
+    agent_description = 'Agent modelled around human driving behavior'
+
+    '''
+    Constructor
+    '''
+    def __init__(self):
+        self.orientation = 0
+        self.x_pos = 0
+        self.y_pos = 0
+        self.x_vel = 0
+        self.y_vel = 0
+        self.speed = 0
+        self.map_graph = None
+        self.vision_objects = []
+
+    '''
+    Called by the vehicle class to update the agent precepts
+    '''
+    def update_precepts(self, orientation, x_pos,y_pos,x_vel,y_vel, map_graph, vision_objects):
+        self.orientation = orientation
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.x_vel = x_vel
+        self.y_vel = y_vel
+        self.speed = math.sqrt(self.x_vel**2 + self.y_vel**2)
+        self.map_graph = map_graph
+        self.vision_objects = vision_objects
+
+    '''
+    This function is called by the vehicle class when it is time to decide what to do
+    
+    The logic for the agent should go here, as follows:
+    1. Use the self variables to determine what the vehicle should do.    
+    2. Queue all desired actions in the actions_queue
+
+    Once this funciton finishes, the actions will then be adjusted as appropriate
+    for the vehicle type and then sent to the vehicle
+    
+    Allowable actions are:
+    ["STOP",0,0]
+    ["FORWARD",SPEED(0-100%),0]
+    ["REVERSE",SPEED(0-100%),0]
+    ["LEFT",AMOUNT(0-100%),0]
+    ["RIGHT",AMOUNT(0-100%),0]
+    ["TARGET",X Coordinate,Y Coordinate]
+    '''    
+    def decide_actions():
+        # All actions the agent wishes to take are stored in this queue
+        actions_queue = deque()
+
+        # Check for wall of simulator
+        if (not self.is_car_inbounds):
+            # Decide to stop to avoid hitting wall
+            actions_queue.append(["STOP",0,0])
+            return actions_queue
+            
+        # Check vision objects
+        for vis_obj in self.vision_objects:
+            distanceToObject = self.calculate_distance(self.x_pos, self.y_pos,vis_obj.x_pos, vis_obj.y_pos)
+            if(distanceToObject < 150):
+                # Decide to stop to avoid collision with a vision object
+                actions_queue.append(["STOP",0,0])
+                return actions_queue
+
+        # Not going to hit anything, so drive forward at 100%
+        actions_queue.append(["FORWARD",100,0])
+
+        # Return actions_queue
+        return actions_queue
+    
+    # Check car is not going to hit a vision object
+    def check_vision_ojects(self):
+
+
+    '''
+    Calculate distance from (x1,y1) to (x2,y2)
+    '''
+    def calculate_distance(self, x1,y1,x2,y2):
+        return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+    '''
+    Check car is within bounds
+    '''
+    def is_car_inbounds(self):        
+        border = 100;
+        x_max = 1200 - border;
+        x_min = 0 + border;
+        y_max = 900 - border;
+        y_min = 0 + border;
+        
+        # whilst in bounds, drive at constant speed
+        if self.x_Pos > x_min and self.x_Pos < x_max and self.y_Pos > y_min and self.y_Pos < y_max:
+            return True
+        else:
+            return False
---
+++ core/vehicle.py
@@ -0,0 +1,146 @@
+from core.protocol import *
+from agents import Human
+from vehicles import Car
+import math
+
+#An object of the vehicle class controls a specific car
+#By calling functions within this class commands are sent to the car
+class Vehicle:
+    def __init__(self, address, socket, agent, vehicle):
+
+        # The unique identifier for this car
+        self.address = address
+
+        # The agent which controls this vehicle's actions
+        if (agent == "Human"):
+            self.agent = Human()
+        else :
+            print("Invalid agent. Must be 'Human'.")
+
+        # The vehicle type which defines parameters such as vehicle speed, acceleration and turning circle
+        if (vehicle == "Car"):
+            self.vehicle = Car()
+        else :
+            print("Invalid vehicle. Must be 'Car'.")
+
+        # Observed parameters of the car
+        self.Orientation = 0
+        self.X_Pos = 0
+        self.Y_Pos = 0      
+        self.X_Vel = 0
+        self.Y_Vel = 0
+        
+        # Precepts of the car - for the moment this is everything
+        # TODO: Filter precepts
+        self.Vision_Objects = []
+
+        self.acceleration = 0
+        self.steering = 0
+
+        # Time car was last seen, for tracking overall system delay
+        self.preceptTime = 0
+
+        # Defines the rounded rectange path which the car will follow
+        # TODO: Use the graph instead
+        self.targets = [(180,450),(250,630),(425,730),(790,730),(970,630),(1030,450),(970,270),(790,170),(445,170),(250,260)]
+        self.currentTarget = 0
+        
+        # Create the client socket
+        self.socket = socket
+
+        # Create dictionary for storing output messages
+        self.out_dict = dict()
+
+    # Queue given command with associated time
+    def queueCommand(self, command):
+        if (command not in self.out_dict):
+            self.out_dict[command] = self.preceptTime
+
+    # Update time car was last seen, for tracking overall system delay
+    def updatePreceptTime(self, newTime):
+        self.preceptTime = newTime
+
+    # Update this cars precepts
+    # TODO: Add to agent
+    def updatePrecepts(self, visionObjects):
+        self.Vision_Objects = visionObjects
+    
+    def updateStateFromVisionObject(self,stateVariables):
+        self.Orientation = stateVariables.Orientation
+        self.X_Pos = stateVariables.X_Pos
+        self.Y_Pos = stateVariables.Y_Pos      
+        self.X_Vel = stateVariables.X_Vel
+        self.Y_Vel = stateVariables.Y_Vel
+
+    def getOrientationToTarget(self):
+        displacementX = self.targets[self.currentTarget][0] - self.X_Pos
+        displacementY = self.targets[self.currentTarget][1] - self.Y_Pos
+
+        displacementOrientation = (90 - 180/math.pi*math.atan2(displacementY, displacementX));
+        if (displacementOrientation < 0):
+                displacementOrientation = 360 + displacementOrientation;
+				
+        return displacementOrientation
+
+    def calculateSpeed(self):
+        return math.sqrt(self.X_Vel**2 + self.Y_Vel**2)
+    
+    # desiredOrientation - number from 0 to 359
+    def orientationControl(self, desiredOrientation):
+        
+        requiredSteering = 0
+        
+        # Can't do anything if we are too slow as the orientation value is incorrectly reported as zero
+        if(self.calculateSpeed() < 25):
+            return
+        
+        #print ('### START ORIENTATION CONTROL ###')
+        
+        errorAngle = ((((desiredOrientation - self.Orientation) % 360) + 540) % 360) - 180;
+        #print ('Current: %d, Desired %d, errorAngle: %d' % (self.Orientation, desiredOrientation, errorAngle))
+        sensitivity = 0.4
+        requiredSteering = int(errorAngle * sensitivity)
+
+        # Upper and lower bound the amount of steering
+        maxSteering = 50
+        if(requiredSteering > maxSteering):
+            requiredSteering = maxSteering
+        elif(requiredSteering < -maxSteering):
+            requiredSteering = -maxSteering
+
+        # Only send steering command if we need to change
+        if (self.steering != requiredSteering):
+            self.steering = requiredSteering
+            # Send steering command
+            if self.steering != 0:
+                #print ('Steering with intensity %d' % self.steering)
+                self.queueCommand(bytes([STEERING, self.steering & 0x7f]))
+            else:
+                #print ('Steering STRAIGHT')
+                self.queueCommand(bytes([STEERING, 0]))
+                
+    def checkCarAtTarget(self):        
+        distanceToTarget = self.distance(self.X_Pos, self.Y_Pos,self.targets[self.currentTarget][0], self.targets[self.currentTarget][1])
+        if(distanceToTarget < 50):
+            self.currentTarget = (self.currentTarget + 1) % len(self.targets)
+
+    def horn(self):
+        self.queueCommand(bytes([HORN, HORN_ON]))
+        sleep(0.5)
+        self.queueCommand(bytes([HORN, HORN_OFF]))
+
+    def headlight_on(self):
+        self.queueCommand(bytes([HEADLIGHT, HEADLIGHT_BRIGHT]))
+        
+    def headlight_off(self):
+        self.queueCommand(bytes([HEADLIGHT, HEADLIGHT_OFF]))
+
+    def run(self):
+        if(self.acceleration != 20):
+            self.acceleration=20
+            self.queueCommand(bytes([THROTTLE, self.acceleration & 0x7f]))
+    
+    def stop(self):
+        if(self.acceleration != 0):
+            self.acceleration=0
+            self.queueCommand(bytes([THROTTLE, 0]))
